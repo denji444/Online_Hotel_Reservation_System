4c2b8668348ffca596c3f0cb6ac93675be783b7b
denji444 <bilalsaleem192005@gmail.com>
2024-05-13 18:25:46 +0500

    Added: a class Room for showing available rooms function.

--- main.cpp
+++ main.cpp
@@ -36,20 +36,52 @@ public:
     }
 };
 
+class Room {
+private:
+    string type;
+    double rate;
+    bool available;
+    vector<Inventory> inventory;
+public:
+    Room(const string& type, double rate) : type(type), rate(rate), available(true) {}
+
+    void addInventory(const Inventory& inv) {
+        inventory.push_back(inv);
+    }
+
+    string getName() const {
+        return type;
+    }
+
+    double getRate() const {
+        return rate;
+    }
+};
+
 // Example usage in the main function
 int main() {
-    // Create an Admin object
-    Admin admin("admin", "admin123");
+     // Create an Admin object
+            Admin admin("admin", "admin123");
 
-    // Access and print the username and password
-    cout << "Admin Username: " << admin.getUsername() << endl;
-    cout << "Admin Password: " << admin.getPassword() << endl;
+     // Access and print the username and password
+            cout << "Admin Username: " << admin.getUsername() << endl;
+            cout << "Admin Password: " << admin.getPassword() << endl;
 
     // Create an Inventory object
-    Inventory item(10, 2000);
-    cout << "GETTING AVAILABLE ROOMS QUANTITY AND THEIR PRICES..." << endl;
-    cout << "Item Quantity: " << item.getQuantity() << endl;
-    cout << "Item Price: $" << item.getPrice() << endl;
+            Inventory item(10, 2000);
+            cout << "GETTING AVAILABLE ROOMS QUANTITY AND THEIR PRICES..." << endl;
+            cout << "Item Quantity: " << item.getQuantity() << endl;
+            cout << "Item Price: $" << item.getPrice() << endl;
+
+    // Create a Room object
+            cout << "Rooms are available... :" << endl;
+            Room room("Luxurious", 3000);
+            cout << "Room Name: " << room.getName() << endl;
+            cout << "Room Rate: $" << room.getRate() << "/night" << endl;
+                     Room room1("Standerd", 1000);
+            cout << "Room Name: " << room1.getName() << endl;
+            cout << "Room Rate: $" << room1.getRate() << "/night" << endl;
+   
 
     return 0;
 }
