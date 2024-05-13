3fb8eedbc366d93b14a9134bac4cf99c55bfbb5b
denji444 <bilalsaleem192005@gmail.com>
2024-05-13 18:19:51 +0500

    Added: a class Inventory for tracking available rooms function.

--- main.cpp
+++ main.cpp
@@ -20,6 +20,22 @@ public:
     }
 };
 
+class Inventory {
+private:
+    int quantity;
+    double price;
+public:
+    Inventory(int quantity, double price) : quantity(quantity), price(price) {}
+
+    int getQuantity() const {
+        return quantity;
+    }
+
+    double getPrice() const {
+        return price;
+    }
+};
+
 // Example usage in the main function
 int main() {
     // Create an Admin object
@@ -28,6 +44,12 @@ int main() {
     // Access and print the username and password
     cout << "Admin Username: " << admin.getUsername() << endl;
     cout << "Admin Password: " << admin.getPassword() << endl;
+
+    // Create an Inventory object
+    Inventory item(10, 2000);
+    cout << "GETTING AVAILABLE ROOMS QUANTITY AND THEIR PRICES..." << endl;
+    cout << "Item Quantity: " << item.getQuantity() << endl;
+    cout << "Item Price: $" << item.getPrice() << endl;
 
     return 0;
 }
